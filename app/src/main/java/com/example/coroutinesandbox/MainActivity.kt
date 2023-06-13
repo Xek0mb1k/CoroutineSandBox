52d20f911a6fa6122ab31e737293ff8f939da958
Xekombik <oganes969@gmail.com>
2023-06-13 16:11:26 +0300

    callback impl

--- app/src/main/java/com/example/coroutinesandbox/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesandbox/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.coroutinesandbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.core.view.isVisible
 import com.example.coroutinesandbox.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,28 +25,69 @@ class MainActivity : AppCompatActivity() {
     private fun loadData() {
         binding.progressBar.isVisible = true
         binding.downloadButton.isEnabled = false
-        val city = loadCity()
-        binding.cityTextView.text = city
-        val temp = loadTemp(city)
-        binding.tempTextView.text = temp.toString()
-        binding.progressBar.isVisible = false
-        binding.downloadButton.isEnabled = true
+        loadCity{ city: String ->
+            binding.cityTextView.text = city
 
-
+            loadTemp(city){ temp: Int ->
+                loadHumidity(city){
+                    Log.d("debug", it.toString())
+                    loadPressure(city){
+                        Log.d("debug", it.toString())
+                        loadVisibility(city){
+                            Log.d("debug", it.toString())
+                            loadWind(city){
+                                Log.d("debug", it.toString())
+                                binding.tempTextView.text = temp.toString()
+                                binding.progressBar.isVisible = false
+                                binding.downloadButton.isEnabled = true
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
-
-    private fun loadCity(): String {
-        Thread.sleep(3000)
-        return "Moscow"
+    private fun loadCity(callBack: (String) -> Unit) {
+        thread {
+            Thread.sleep(3000)
+            callBack.invoke("Moscow")
+        }
     }
 
-    private fun loadTemp(city: String): Int {
+    private fun loadTemp(city: String, callBack: (Int) -> Unit) {
 
         Toast.makeText(this, "Loading temp from this city: $city", Toast.LENGTH_SHORT)
             .show()
-        Thread.sleep(2000)
-        return 17
+        Thread.sleep(1000)
+        callBack.invoke(17)
     }
 
+    private fun loadPressure(city: String, callBack: (Int) -> Unit){
+        Toast.makeText(this, "Loading PRESSURE from this city: $city", Toast.LENGTH_SHORT)
+            .show()
+        Thread.sleep(1000)
+        callBack.invoke(500)
+    }
+
+    private fun loadHumidity(city: String, callBack: (Int) -> Unit){
+        Toast.makeText(this, "Loading temp from this city: $city", Toast.LENGTH_SHORT)
+            .show()
+        Thread.sleep(1000)
+        callBack.invoke(17)
+    }
+
+    private fun loadVisibility(city: String, callBack: (Int) -> Unit){
+        Toast.makeText(this, "Loading visibility from this city: $city", Toast.LENGTH_SHORT)
+            .show()
+        Thread.sleep(1000)
+        callBack.invoke(2000)
+    }
+
+    private fun loadWind(city: String, callBack: (Int) -> Unit){
+        Toast.makeText(this, "Loading wind from this city: $city", Toast.LENGTH_SHORT)
+            .show()
+        Thread.sleep(3000)
+        callBack.invoke(5)
+    }
 }
